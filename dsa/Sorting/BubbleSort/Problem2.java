1206cb05a9ffb2d9b31efbecfc4f8e290b2b29f7
Siddhartha <srisaisiddharthamanepalli@gmail.com>
2025-10-22 10:33:57 +0530

    Added Selection Sort

--- dsa/Sorting/BubbleSort/Problem2.java
+++ dsa/Sorting/BubbleSort/Problem2.java
@@ -1,37 +1,32 @@
 package dsa.Sorting.BubbleSort;
 
+
 public class Problem2 {
     public static void main(String[] args) {
-        String str = "csiplearninghub";
-        char[] arr = str.toCharArray(); // convert string to char array
-
-        int n = arr.length;
-        boolean swapped;
-
-        // Bubble sort in descending order
-        for (int i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] < arr[j + 1]) { // '<' for descending order
-                    // swap
-                    char temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-            if (!swapped) break; // optimization: stop if already sorted
-        }
-
-        // Print the sorted array
+        String alp = "csiplearninghub";
+        BubbleSort(alp);
         System.out.print("[");
-        for (int i = 0; i < n; i++) {
-            System.out.print("'" + arr[i] + "'");
-            if (i != n - 1)
-                System.out.print(", ");
+        for(int i = 0; i < alp.length(); i++){
+            System.out.print(alp.charAt(i) + " ");
         }
         System.out.println("]");
     }
-}
-
+    static void BubbleSort(String alp){
+        boolean swapped;
+        char[] arr = alp.toCharArray();
+        for(int i = 0; i < arr.length; i++){
+            swapped = false;
+            for(int j = 1; j < arr.length - i; j++){
+                if(arr[j] > arr[j-1]){
+                    char temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+            break; 
+            }
+        }
+    }
 }
